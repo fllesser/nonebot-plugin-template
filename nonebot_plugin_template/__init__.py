db11b08aeb8b057adf9c4fb0e5c9cd3190ed171d
fllesser <fllessive@gmail.com>
2025-03-07 14:28:53 +0800

    :sparkles: chore: require(waiter)

--- nonebot_plugin_template/__init__.py
+++ nonebot_plugin_template/__init__.py
@@ -1,6 +1,7 @@
 from nonebot import logger, require
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 
+require("nonebot_plugin_waiter")
 require("nonebot_plugin_uninfo")
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_localstore")
