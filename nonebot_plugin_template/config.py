02c8b6800453e3a486c0d3dab2a134cbe814b5f7
fllesser <fllessive@gmail.com>
2025-03-31 18:35:30 +0800

    :arrow_up: chore(deps): specify version ranges for dependencies in pyproject.toml and uv.lock, update NICKNAME initialization in config.py

--- nonebot_plugin_template/config.py
+++ nonebot_plugin_template/config.py
@@ -11,4 +11,4 @@ plugin_config: Config = get_plugin_config(Config)
 global_config = get_driver().config
 
 # 全局名称
-NICKNAME: str | None = next(iter(global_config.nickname), None)
+NICKNAME: str = next(iter(global_config.nickname), "")
